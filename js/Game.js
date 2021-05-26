2fe3f0766c87c48813b9837ce98fe3e3a5bbc527
Caz Gabrielse <cgabrielse@outlook.com>
2021-05-26 18:28:36 -0500

    Added phrase/game constructors and methods
    Includes an array of phrase objects and a start game method that retrieves a random phrase from said array.

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,30 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+class Game {
+    constructor() {
+        this.missed = 0;
+        this.phrases = [{
+            phrase: 'Hello World' 
+        }, {
+            phrase: 'Cherry Pie'
+        }, {
+            phrase: 'Alley Cat'
+        }, {
+            phrase: 'Yellow Submarine'
+        }, {
+            phrase: 'Kamehameha'
+        }];
+        this.activePhrase = null;
+    }
+
+    startGame() {
+        document.querySelector('#overlay').style.display = 'none';
+        this.activePhrase = new Phrase(this.getRandomPhrase());
+        this.activePhrase.addPhraseToDisplay();
+    }
+
+    getRandomPhrase() {
+        return this.phrases[~~(Math.random() * this.phrases.length)];
+    }
+
